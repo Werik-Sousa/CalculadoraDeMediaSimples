3e6c3fb3a0a0f67bcd18994cf3cb35a7a837b2e9
Werik Sousa <werikvsousa@gmail.com>
2022-10-07 14:38:05 -0300

    Calculadora de media simples

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace MediaDaTurma {
+    partial class Form1 {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.Nota1 = new System.Windows.Forms.Label();
+            this.Nota2 = new System.Windows.Forms.Label();
+            this.Nota3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Média = new System.Windows.Forms.Label();
+            this.Resultado = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Nota1
+            // 
+            this.Nota1.AutoSize = true;
+            this.Nota1.Location = new System.Drawing.Point(35, 35);
+            this.Nota1.Name = "Nota1";
+            this.Nota1.Size = new System.Drawing.Size(54, 20);
+            this.Nota1.TabIndex = 0;
+            this.Nota1.Text = "Nota1";
+            // 
+            // Nota2
+            // 
+            this.Nota2.AutoSize = true;
+            this.Nota2.Location = new System.Drawing.Point(35, 106);
+            this.Nota2.Name = "Nota2";
+            this.Nota2.Size = new System.Drawing.Size(54, 20);
+            this.Nota2.TabIndex = 1;
+            this.Nota2.Text = "Nota2";
+            // 
+            // Nota3
+            // 
+            this.Nota3.AutoSize = true;
+            this.Nota3.Location = new System.Drawing.Point(35, 193);
+            this.Nota3.Name = "Nota3";
+            this.Nota3.Size = new System.Drawing.Size(54, 20);
+            this.Nota3.TabIndex = 2;
+            this.Nota3.Text = "Nota3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(161, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(161, 103);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(130, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(161, 190);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(130, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // Média
+            // 
+            this.Média.AutoSize = true;
+            this.Média.Location = new System.Drawing.Point(35, 269);
+            this.Média.Name = "Média";
+            this.Média.Size = new System.Drawing.Size(56, 20);
+            this.Média.TabIndex = 6;
+            this.Média.Text = "Média";
+            // 
+            // Resultado
+            // 
+            this.Resultado.AutoSize = true;
+            this.Resultado.Location = new System.Drawing.Point(35, 349);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(86, 20);
+            this.Resultado.TabIndex = 7;
+            this.Resultado.Text = "Resultado";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(184, 269);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 20);
+            this.label6.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Coral;
+            this.label7.Location = new System.Drawing.Point(184, 349);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 20);
+            this.label7.TabIndex = 9;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.Cyan;
+            this.btnCalcular.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCalcular.Location = new System.Drawing.Point(38, 400);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(253, 23);
+            this.btnCalcular.TabIndex = 10;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 450);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Resultado);
+            this.Controls.Add(this.Média);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Nota3);
+            this.Controls.Add(this.Nota2);
+            this.Controls.Add(this.Nota1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Nota1;
+        private System.Windows.Forms.Label Nota2;
+        private System.Windows.Forms.Label Nota3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label Média;
+        private System.Windows.Forms.Label Resultado;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaDaTurma {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e) {
+            double nota1, nota2, nota3, media;
+
+            nota1 = double.Parse(textBox1.Text);
+            nota2 = double.Parse(textBox2.Text);
+            nota3 = double.Parse(textBox3.Text);
+
+            media = (nota1 + nota2 + nota3) / 3;
+
+            label6.Text = Convert.ToString(media);
+
+            if (media > 7) {
+                label7.Text = "Aprovado";
+                label7.ForeColor = Color.Blue;
+            } else {
+                label7.Text = "Reprovado";
+                label7.ForeColor = Color.Red;
+            }
+        }
+    }
+}
